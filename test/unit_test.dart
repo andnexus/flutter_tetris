7b4fcdc9b1113589b21171804cc0456a4b9011d0
Benjamin Lehejcek <benjamin.lehejcek@ext.iteratec.com>
2022-12-03 10:53:54 +0100

    Vector tests

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -4,11 +4,29 @@ import 'package:tetris/game/vector.dart';
 void main() {
   group('Unit tests', () {
     group('Vector', () {
-      test('Addition', () async {
+      test('zero', () {
+        expect(Vector.zero.x == 0 && Vector.zero.y == 0, true);
+      });
+      test('+', () {
         expect(const Vector(1, 1) + const Vector(1, 1), const Vector(2, 2));
       });
-      test('Subtraction', () async {
+      test('-', () {
         expect(const Vector(1, 1) - const Vector(1, 1), Vector.zero);
+      });
+      test('*', () {
+        expect(const Vector(1, 1) * Vector.zero, Vector.zero);
+      });
+      test('<', () {
+        expect(Vector.zero < const Vector(1, 1), true);
+      });
+      test('>=', () {
+        expect(Vector.zero >= const Vector(-1, -1), true);
+      });
+      test('==', () {
+        expect(Vector.zero, Vector.zero);
+      });
+      test('toString', () {
+        expect(Vector.zero.toString(), '0,0');
       });
     });
   });
