d2c953d30bb0b8a006e5db3715ccac0677aac42b
Benjamin Lehejcek <benjamin.lehejcek@ext.iteratec.com>
2022-12-03 12:34:17 +0100

    GitHub actions coverage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tetris/main.dart';
 
@@ -6,6 +7,8 @@ void main() {
     testWidgets('Find text', (tester) async {
       await tester.pumpWidget(const App());
       await tester.pumpAndSettle();
+      await tester.sendKeyEvent(LogicalKeyboardKey.keyD);
+      await tester.idle();
       expect(find.text('HOLD'), findsOneWidget);
     });
   });
